5cc898686201e3ef7d2d642b10bfea95b09b89a9
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Pro.local>
2020-08-31 08:21:40 +0600

    data loading with axios

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { Button } from '@material-ui/core';
 import './App.css';
 import News from './components/News/News';
-
+import axios from 'axios';
 
 function App() {
   const [articles, setArticles] = useState([]);
+  // useEffect( () => {
+  //   const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=2ec5c4fc314c4e6595ffe215903d3d18'
+  //   fetch(url)
+  //   .then(res => res.json())
+  //   .then(data => setArticles(data.articles))
+  // } , [])
+
+// using axios for data loading
+
   useEffect( () => {
     const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=2ec5c4fc314c4e6595ffe215903d3d18'
-    fetch(url)
-    .then(res => res.json())
-    .then(data => setArticles(data.articles))
-  } , [])
+    axios(url)
+    .then(data => setArticles(data.data.articles))
+  }, [])
+
   return (
     <div >
      

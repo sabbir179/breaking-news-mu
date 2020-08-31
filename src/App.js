9bacbf9de0f1e466027d0563bd05c139812e679c
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Pro.local>
2020-08-31 06:29:43 +0600

    react js practic project start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <Button color="primary">Hello World</Button>
+      <Button variant="contained" color="secondary">Secondary</Button>
+    </div>
+  );
+}
+
+export default App;
